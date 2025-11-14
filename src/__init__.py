cb5bac0546a8d331ada9e2ccf8c7054d5c036506
Harry Herald R <harryherald019@gmail.com>
2025-11-14 21:41:08 +0530

    ok just please go to github....... internet :sign......im sorry i forgot to add the gitignore files :(

--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-# Make src a package and expose common modules
-from . import scanner, db, report_generator, analytics, data_prep
--- src/test.py
+++
@@ -1,5 +0,0 @@
-from zapv2 import ZAPv2
-
-zap = ZAPv2(apikey='', proxies={'http': 'http://localhost:8090', 'https': 'http://localhost:8090'})
-print("ZAP Version:", zap.core.version)
-    
